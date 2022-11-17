3e4252c668c1451b6097e1e7baa09b607adf09be
Duarte Rocha <duarte.rocha@kelvininc.com>
2022-11-17 23:11:58 +0000

    Secret santa app

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # secret-santa
+
+Simple python script that takes a csv file with Name,Phone and randomly assigns a secret santa to 
+everyone and sends a SMS to everyone letting them know. This leverages AWS SNS for sending the text message.
+
+## csv file format
+
+```
+Name,Phone Number
+John Doe,987654321
+```
+
+# AWS -> SNS -> Text messaging (SMS)
+Usualy the account will in SMS sandbox mode, to allow to send to any number disable sandbox mode.
---
+++ secret-santa.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import csv
+import boto3
+import random
+import logging
+
+AWS_ACCESS_KEY="AWS-ACCESS-KEY"
+AWS_SECRET_KEY="AWS-SECRET-KEY"
+AWS_REGION="eu-west-1"
+
+FILE="list.csv"
+
+budget = "20"
+coin = "€"
+country_prefix="+351"
+
+# Change to ERROR to not print anything
+logging.basicConfig(filename="results.log", level=logging.DEBUG)
+
+participants_list = []
+
+#### Load list from csv
+with open(FILE, mode='r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    fields = next(csv_reader)
+    for row in csv_reader:
+        participants_list.append(row)
+    participants_number = csv_reader.line_num - 1
+    csv_file.close()
+
+# Create an SNS client
+client = boto3.client(
+    "sns",
+    aws_access_key_id=AWS_ACCESS_KEY,
+    aws_secret_access_key=AWS_SECRET_KEY,
+    region_name=AWS_REGION
+)
+
+random.shuffle(participants_list)
+logging.debug(participants_list)
+offset = [participants_list[-1]] + participants_list[:-1]
+for santa, receiver in zip(participants_list, offset):
+    logging.info(santa[0]+ " buys for " + receiver[0])
+    sms_message="Secret Santa 2022!!!\nYou're the Secret Santa of <" + receiver[0] + ">.\nThe gift's budget is: " + budget + coin
+    logging.debug(sms_message)
+    logging.debug("Sending sms to " + santa[0] + " nº <"+ santa[1].replace(" ", "") +">")
+    if santa[1].replace(" ", "") == "934910217" :
+        logging.debug("Sending SMS")
+        client.publish(
+            PhoneNumber = country_prefix + santa[1].replace(" ", ""),
+            Message=sms_message
+        )
